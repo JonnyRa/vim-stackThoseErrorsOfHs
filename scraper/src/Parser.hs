6a2f6074780693cc84cd5766adc9c72989921c90
Jonny Ramsden <jonathan.ramsden@tracsis.com>
2024-01-08 16:58:16 +0000

    started writing code to collect up the error messages we've gathered

--- scraper/src/Parser.hs
+++ scraper/src/Parser.hs
@@ -3,24 +3,54 @@ module Parser
 )
 where
 
+import Data.List.Extra ((!?))
+import Data.Tuple.Extra
+
 data ParseState = ParseState {
   _currentParser :: Parser
+, _errorsInReverseOrder :: [ErrorInformation]
 }
 
 data Parser = 
     WaitingForError
   | GatheringErrorMessage [String]
 
+data ErrorInformation = ErrorInformation {
+
+}
+
 convertStackOutput :: String -> String
-convertStackOutput allInput = undefined $ foldr processLine (ParseState WaitingForError) $ lines allInput
+convertStackOutput allInput = undefined $ foldr processLine (ParseState WaitingForError []) $ lines allInput
   where
   processLine :: String -> ParseState -> ParseState
-  processLine line = parseLine . _currentParser
+  processLine line currentState = parseLine $ _currentParser currentState
     where
     lineContent :: [String]
     lineContent = words line
     parseLine :: Parser -> ParseState
     parseLine WaitingForError =
       if any (`elem` ["error:", "warning:"]) lineContent
-      then ParseState $ GatheringErrorMessage lineContent
-      else ParseState WaitingForError
+      then changeToParser (GatheringErrorMessage lineContent) currentState
+      else changeToParser WaitingForError currentState
+
+    parseLine (GatheringErrorMessage errorLine) =
+      ParseState WaitingForError $ makeInformation errorLine lineContent: _errorsInReverseOrder currentState
+
+changeToParser :: Parser -> ParseState -> ParseState
+changeToParser parser state = state {_currentParser = parser}
+
+makeInformation :: [String] -> [String] -> ErrorInformation
+makeInformation errorLine firstErrorMessageLine =
+  ErrorInformation
+
+  where
+  adjustedError, adjustedMessage :: [String]
+  (adjustedError, adjustedMessage) = 
+    adjustment (errorLine, firstErrorMessageLine)
+    where
+    adjustment :: ([String], [String]) -> ([String], [String])
+    adjustment = 
+      if errorLine !? 1 == Just ">"
+      then both (drop 2) 
+      else id
+        
