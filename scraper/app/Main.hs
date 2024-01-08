62c92b17d68bf4769f1714672f081938ab086260
Jonny Ramsden <jonathan.ramsden@tracsis.com>
2024-01-08 17:47:15 +0000

    do something with the output

--- scraper/app/Main.hs
+++ scraper/app/Main.hs
@@ -7,6 +7,7 @@ main :: IO ()
 main = do
   args <- getArgs
   let inputFile = head args
+  let outputFile = last args
   lazyContents <- readFile inputFile
-  pure $ convertStackOutput lazyContents
-  pure ()
+  let output = convertStackOutput lazyContents
+  writeFile outputFile output
