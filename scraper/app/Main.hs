649166a61dfbee4d1d1c9d053cf08731921d0218
Jonny Ramsden <jonathan.ramsden@tracsis.com>
2024-01-08 18:06:56 +0000

    do some basic usage info/args checking

--- scraper/app/Main.hs
+++ scraper/app/Main.hs
@@ -2,10 +2,16 @@ module Main (main) where
 
 import Parser
 import System.Environment
+import System.Exit
+import Control.Monad
 
 main :: IO ()
 main = do
   args <- getArgs
+  when (length args /= 2) $ do
+    putStrLn "Error needs to 2 arguments: inputFile outputFile"
+    exitFailure
+
   let inputFile = head args
   let outputFile = last args
   lazyContents <- readFile inputFile
